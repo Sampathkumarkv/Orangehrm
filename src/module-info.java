102b447f02582fce75d8c5162d34415427acddcc
Gensys Info X <Gensys Info X@DESKTOP-M8E917U>
2025-11-07 10:56:03 +0530

    I have created a new demo2 class

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author SRI
- *
- */
-module orange {
-}
---
+++ src/orange/demo2.java
@@ -0,0 +1,10 @@
+package orange;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
